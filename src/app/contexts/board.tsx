602346e7345e19e00e5836ba3a173fa23e38f921
vitor.domingues.dev <123842676+vitordmgdev@users.noreply.github.com>
2025-03-28 17:05:58 -0300

    feat: create Task added to boardContext

--- src/app/contexts/board.tsx
+++ src/app/contexts/board.tsx
@@ -1,13 +1,11 @@
 "use client";
 
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 type Task = {
     id: string,
     name: string,
     description?: string,
-    createdAt: Date,
-    updatedAt: Date
 };
 
 type Column = {
@@ -25,13 +23,14 @@ type Board = {
 //Tipagem do BoardContext
 type BoardContextType = {
     board: Board,
+    createTask: (columnId: string, task: Task) => void
 };
 
 const BoardContext = createContext<BoardContextType | undefined>(undefined);
 
 //Estado inicial do quadro
 const initialBoard = {
-    id: 1,
+    id: "1",
     name: "Meu quadro de tarefas",
     description: "Tarefas do dia-a-dia",
     columns: [
@@ -44,9 +43,24 @@ const initialBoard = {
 export const BoardProvider = ({children}:{children:React.ReactNode}) => {
     const [ board, setBoard ] = useState<Board>(initialBoard);
 
+    const createTask = (columnId: string, task: Task ) => {
+        setBoard((prev) => ({
+            ...prev,
+            columns: prev.columns.map((col) =>
+                col.id === columnId ? {...col, tasks: [...col.tasks, task]} : col
+            ),
+        }));
+    };
+
     return (
-        <BoardContext.Provider value={{ board, setBoard, }}>
+        <BoardContext.Provider value={{ board, createTask }}>
             {children}
         </BoardContext.Provider>
     );
+};
+
+export const useBoard = () => {
+    const context = useContext(BoardContext);
+    if (!context) throw new Error("useBoard deve ser usado dentro de um BoardProvider");
+    return context;
 };
